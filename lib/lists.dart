2a5eb2eb27bf1ef6fe93ddca53740b922ca44943
NitroZeus774 <nitrozeusl774@gmail.com>
2024-05-20 15:07:15 -0500

    arreglar problemas del codigo

--- lib/lists.dart
+++ lib/lists.dart
@@ -2,9 +2,9 @@ void main() {
   var alm = ['isr', 'susan', 'serg', 'hugo'];
   List<int> lent = [1, 2, 4, 5, 7];
   print(lent);
-  alm.forEach((lum) {
+  for (var lum in alm) {
     print(lum);
-  });
+  }
   print(alm.reversed);
   print(alm.first);
   print(alm.last);
